e3439dbc264b41530d08d6b98edf522a3806db9b
scragly <29337040+scragly@users.noreply.github.com>
2018-01-01 09:01:40 +1000

    pokemon completion

--- eevee/cogs/pokemon/pokemon.py
+++ eevee/cogs/pokemon/pokemon.py
@@ -4,8 +4,8 @@ import os
 import discord
 from discord.ext import commands
 
-from eevee import command, checks
-from eevee.utils import make_embed, fuzzymatch
+from eevee import group
+from eevee.utils import make_embed, fuzzymatch, url_color
 
 class Pokemon():
     """Represents a Pokemon"""
@@ -23,7 +23,17 @@ class Pokemon():
     @property
     def is_raid(self):
         """bool : Indicates if the pokemon can show in Raids"""
-        return self.name in self.bot.pkmn_info['raid_list']
+        return self.name in list(self.bot.raid_pokemon.keys())
+
+    @property
+    def raid_level(self):
+        """Returns raid egg level"""
+        return self.bot.raid_pokemon[self.name]["level"] if self.is_raid else None
+
+    def max_raid_cp(self, weather_boost=False):
+        """Returns max CP on capture after raid"""
+        key = "max_cp_w" if weather_boost else "max_cp"
+        return self.bot.raid_pokemon[self.name][key] if self.is_raid else None
 
     @property
     def role(self, guild=None):
@@ -50,9 +60,9 @@ class Pokemon():
         return self.bot.pkmn_info[pkmn]["types"]
 
     @property
-    def type_effects(self, types):
+    def type_effects(self):
         type_eff = {}
-        for _type in types:
+        for _type in self.types:
             for atk_type in self.bot.type_chart[_type]:
                 if atk_type not in type_eff:
                     type_eff[atk_type] = 1
@@ -105,13 +115,52 @@ class Pokedex:
             "index" : "6",
             "description" : "Pokemon Information and Management"
         }
+        self.type_emoji = bot.config.type_emoji
         init_pokedata(bot)
 
-    @command(category="Pokedex")
-    async def pokemon(self, ctx, pokemon: PkmnConverter):
+    def get_type_emoji(self, type):
+        return self.type_emoji[type.lower()]
+
+    @group(category="Pokedex", aliases=["pd"])
+    async def pokedex(self, ctx, pokemon: PkmnConverter):
         """Return pokemon info"""
         if isinstance(pokemon, Pokemon):
-            embed = make_embed()
-            await ctx.send(f'We found "{pokemon.name}".')
+            pkmn_no = str(pokemon.id).zfill(3)
+            pkmn_url = ('https://raw.githubusercontent.com/FoglyOgly/'
+                       f'Meowth/master/images/pkmn/{pkmn_no}_.png')
+            pkmn_colour = await url_color(pkmn_url)
+            embed = make_embed(
+                title=f'#{pkmn_no} - {pokemon.name.capitalize()}',
+                thumbnail=pkmn_url,
+                msg_colour=pkmn_colour)
+            weak_emoji = []
+            for k, v in pokemon.weak_against.items():
+                weak_emoji.append(self.get_type_emoji(k))
+            strong_emoji = []
+            for k, v in pokemon.strong_against.items():
+                strong_emoji.append(self.get_type_emoji(k))
+            type_emoji = []
+            for _type in pokemon.types:
+                type_emoji.append(self.get_type_emoji(_type))
+            
+            embed.add_field(name='Types',
+                            value=' '.join(type_emoji), inline=False)
+            embed.add_field(name='Weak Against',
+                            value=' '.join(weak_emoji), inline=False)
+            embed.add_field(name='Strong Against',
+                            value=' '.join(strong_emoji), inline=False)
+            if pokemon.is_raid:
+                embed.add_field(name='Raid Level',
+                                value=pokemon.raid_level, inline=False)
+                embed.add_field(name='Raid Capture Max CP',
+                                value=pokemon.max_raid_cp(), inline=False)
+                embed.add_field(name='Raid Capture Max CP w/ Weather',
+                                value=pokemon.max_raid_cp(weather_boost=True),
+                                inline=False)
+            await ctx.send(embed=embed)
         else:
             await ctx.send(f'Did you mean "{pokemon}"?')
+
+    # @pokedex.command()
+    # async def raids(self, ctx, level=None):
+        
