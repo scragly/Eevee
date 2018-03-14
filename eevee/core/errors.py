9da65e7449c249efa1c755dbd72e6005ce18c75c
scragly <29337040+scragly@users.noreply.github.com>
2018-03-14 14:49:02 +1000

    add Pokemon errors

--- eevee/core/errors.py
+++ eevee/core/errors.py
@@ -1,4 +1,12 @@
 from discord.ext import commands
+from asyncpg import PostgresError
+
+
+class PokemonNotFound(commands.CommandError):
+    """Exception raised when Pokemon given does not exist."""
+    def __init__(self, pokemon, retry=True):
+        self.pokemon = pokemon
+        self.retry = retry
 
 class MissingSubcommand(commands.CommandError):
     pass
