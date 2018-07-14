2a3142232eb04cde6116d5a352d61d896057f6df
scragly <29337040+scragly@users.noreply.github.com>
2018-07-14 22:20:11 +1000

    unknown event logging

--- eevee/cogs/dev/__init__.py
+++ eevee/cogs/dev/__init__.py
@@ -5,6 +5,8 @@ of the bot and above.
 """
 
 from .cog import Dev
+from .unknown_events import UnknownEventLogging
 
 def setup(bot):
     bot.add_cog(Dev(bot))
+    bot.add_cog(UnknownEventLogging(bot))
