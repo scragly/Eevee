9cfc2c364f334aadc46722e5e7ac7bb1e6084e0c
scragly <29337040+scragly@users.noreply.github.com>
2018-08-21 23:27:47 +1000

    more robust tz matching

--- eevee/cogs/time/cog.py
+++ eevee/cogs/time/cog.py
@@ -13,9 +13,64 @@ class Time(Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tzdburl = 'https://github.com/sdispater/pytzdata/blob/master/pytzdata/_timezones.py'
+        self.timezones = self.get_timezones()
+
+    def get_timezones(self):
+        zone_tab = pytz.open_resource('zone.tab')
+        try:
+            data = {}
+            for line in zone_tab:
+                line = line.decode('UTF-8')
+                if line.startswith('#'):
+                    continue
+                code, coordinates, zone = line.split(None, 4)[:3]
+                if zone not in pytz.all_timezones_set:
+                    continue
+                try:
+                    data[code].append(zone)
+                except KeyError:
+                    data[code] = [zone]
+            self.timezones = data
+        finally:
+            zone_tab.close()
+
+    def timezone_names(self):
+        tznames = {}
+        for tz in pytz.all_timezones:
+            name = pytz.timezone(tz).localize(datetime.datetime.now()).tzname()
+            try:
+                tznames[name].append(tz)
+            except KeyError:
+                tznames[name] = [tz,]
+        return tznames
 
     def match_timezone(self, query):
-        return fuzzymatch.get_match(pytzdata.timezones, query, 60)[0]
+        # try it as-is first
+        try:
+            pytz.timezone(query)
+            return [(query, 100),]
+        except pytz.UnknownTimeZoneError:
+            pass
+
+        # check if in tz names
+        tz_names = self.timezone_names()
+        if query.upper() in tz_names:
+            return [(tz, 100) for tz in tz_names[query.upper()]]
+
+        # fuzzymatch against all timezones as last resort
+        matches = fuzzymatch.get_matches(tz_names.keys(), query, 80)
+
+        commontz_matches = fuzzymatch.get_matches(
+            pytz.common_timezones, query, 90, True)
+
+        if commontz_matches:
+            matches.extend(commontz_matches)
+
+        fullmatches = [(tz, 100) for tz, s in matches if s == 100]
+        if len(fullmatches) == 1:
+            return fullmatches
+
+        return matches
 
     async def get_timezone(self, member_id):
         table = self.bot.dbi.table('member_timezones')
@@ -23,15 +78,58 @@ class Time(Cog):
         table.query.where(member_id=member_id)
         return await table.query.get_value()
 
+    async def verify_timezone(self, ctx, timezone):
+
+        # detect utc offset values
+        try:
+            tzoffset = datetime.timedelta(hours=float(timezone))
+            datetime.timezone(tzoffset)
+        except ValueError:
+            pass
+        else:
+            return timezone
+
+        # try matching if not an offset
+        results = self.match_timezone(timezone)
+
+        if not results:
+            return None
+
+        if len(results) == 1:
+            return results[0][0]
+
+        result_str = [f"{tz} ({s}%)" for tz, s in results]
+        results = [tz for tz, s in results]
+
+        which_msg = await ctx.embed(
+            "Which of these matches?",
+            '\n'.join(result_str),
+            msg_type='help',
+            footer="To stop, reply with 'cancel'.")
+
+        def check(m):
+            return m.channel == ctx.channel and m.author == ctx.author
+
+        reply_msg = await ctx.bot.wait_for('message', check=check)
+
+        await which_msg.delete()
+
+        if reply_msg.content.lower() == 'cancel':
+            return None
+
+        return fuzzymatch.get_match(results, reply_msg.content, 95, True)[0]
+
     @command()
     async def tztest(self, ctx, *, timezone):
-        results = fuzzymatch.get_matches(pytz.all_timezones, timezone)
-        pytz.country_timezones
-        if not results:
-            await ctx.error('No Result')
-        else:
-            results = [f"{s}% - {tz}" for tz, s in results]
-            await ctx.success('Matching with:', '\n'.join(results))
+
+        match = await self.verify_timezone(ctx, timezone)
+
+        if not match:
+            return await ctx.error('No Match')
+
+        tz_time = pendulum.now(match).format('HH:mm, dddd')
+
+        await ctx.success(f'Match: {match} | {tz_time}')
 
     @group(invoke_without_command=True, aliases=['timezone'])
     async def tz(self, ctx, member: discord.Member = None):
@@ -52,22 +150,11 @@ class Time(Cog):
     async def _set(self, ctx, timezone=None, member: discord.Member = None):
         """Sets a member's timezone"""
         member = member or ctx.author
-        try:
-            timezone = float(timezone)
-        except ValueError:
-            pass
-        if isinstance(timezone, float):
-            tzoffset = datetime.timedelta(hours=timezone)
-            try:
-                datetime.timezone(tzoffset)
-            except ValueError as e:
-                return await ctx.error(str(e).title())
-        else:
-            timezone = self.match_timezone(timezone)
+
+        if timezone:
+            timezone = await self.verify_timezone(ctx, timezone)
             if not timezone:
-                return await ctx.error(
-                    'Invalid Timezone Provided.',
-                    f"[List of all available timezones]({self.tzdburl})")
+                return await ctx.error('Invalid Timezone.')
 
         table = ctx.bot.dbi.table('member_timezones')
         table.insert(
@@ -120,7 +207,7 @@ class Time(Cog):
     @time.command(name='tz', aliases=['timezones', 'timezone'])
     async def time_tz(self, ctx, timezone=None):
         timezone = timezone or 'UTC'
-        timezone = self.match_timezone(timezone)
+        timezone = await self.verify_timezone(ctx, timezone)
         try:
             timezone = float(timezone)
         except ValueError:
