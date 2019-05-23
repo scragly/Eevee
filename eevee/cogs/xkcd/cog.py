1a424ff576c4a676b645d735018149894919d8ae
Scragly <29337040+scragly@users.noreply.github.com>
2019-05-24 01:44:24 +1000

    Add table updating.

--- eevee/cogs/xkcd/cog.py
+++ eevee/cogs/xkcd/cog.py
@@ -1,12 +1,80 @@
+from datetime import datetime
+
 from async_timeout import timeout
 
 from eevee import command, Cog
+
+LATEST_URL = "https://xkcd.com/info.0.json"
+ISSUE_URL = "https://xkcd.com/{comic_num}/info.0.json"
 
 
 class XKCD(Cog):
     """Test commands that are open for public usage."""
     def __init__(self, bot):
         self.bot = bot
+        self.table = bot.dbi.table('xkcd')
+
+    async def get_comic(self, issue: int = None):
+        url = ISSUE_URL.format(issue) if issue else LATEST_URL
+        async with self.bot.session.get(url) as r:
+            return await r.json()
+
+    async def latest_id(self):
+        data = await self.get_comic()
+        return data["num"]
+
+    async def update_data(self, feedback_dest=None):
+        if feedback_dest:
+            await feedback_dest.send("Starting Update")
+        query = self.table.query("id").order_by("id", asc=False).limit(1)
+        result = await query.get_one()
+        if not result:
+            result = 0
+
+        if feedback_dest:
+            await feedback_dest.send(f"Latest Stored Comic ID: {result}")
+
+        latest = await self.latest_id()
+        if feedback_dest:
+            await feedback_dest.send(f"Latest Released Comic ID: {latest}")
+
+        if result >= latest:
+            if feedback_dest:
+                await feedback_dest.send(f"Update Finished: No new releases found.")
+            return
+
+        if feedback_dest:
+            update_text = f"Pulling updates for comics {result} to {latest}."
+            update_msg = await feedback_dest.send(update_text + f"\n0/{latest} done.")
+
+        for i in range(result+1, latest+1):
+            data = await self.get_comic(i)
+            self.table.insert.row(
+                id=data['num'],
+                img=data['img'],
+                title=data['title'],
+                safe_title=data['safe_title'],
+                alt=data['alt'],
+                year=data['year'],
+                month=data['month'],
+                day=data['day'],
+                transcript=data['transcript'],
+                news=data['news']
+            )
+
+            if feedback_dest:
+                last_change = update_msg.edited_at or update_msg.created_at
+                since_change = datetime.utcnow() - last_change
+                if since_change.total_seconds() > 5:
+                    await update_msg.edit(update_text + f"\n{data['num']}/{latest} done.")
+
+        if feedback_dest:
+            await update_msg.edit(update_text + f"\n{latest}/{latest} done.")
+
+        self.table.insert.commit(do_update=False)
+
+        if feedback_dest:
+            await feedback_dest.send(f"Update Finished.")
 
     @command()
     async def xkcd(self, ctx, comic_number: int = None):
@@ -19,3 +87,7 @@ class XKCD(Cog):
                  f"{xkcd_data['num']} - "
                  f"{xkcd_data['year']}/{xkcd_data['month']}/{xkcd_data['day']}")
         await ctx.embed(title, footer=xkcd_data['alt'], image=xkcd_data['img'])
+
+    @command()
+    async def xkcd_update(self, ctx):
+        await self.update_data(ctx)
